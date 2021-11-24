82b8b276783a0ec326dde511aea82d04d6f95256
grahamguthrie99 <grahamguthrie99@gmail.com>
2021-11-23 19:46:25 -0500

    Finished buildspace tutorial

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,50 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-      ["Leo", "Aang", "Pikachu"],       // Names
-      ["https://i.imgur.com/pKd5Sdk.png", // Images
-      "https://i.imgur.com/xVu4vFL.png", 
-      "https://i.imgur.com/WMB6g9u.png"],
-      [100, 200, 300],                    // HP values
-      [100, 50, 25]                       // Attack damage values
+      [ "Ursula Trujillo", 
+        "Katrina Cabrera", 
+        "Dewey Arroyo",
+        "Tyler Allison",
+        "Cherry Parsons",
+        "Jeffry Lawrence",
+        "Tristan Pierce", 
+        "Deana Brandt", 
+        "Chad Robinson", 
+        ], // Names
+      [ "https://i.imgur.com/xlpIlRi.png", 
+        "https://i.imgur.com/aHuqnUy.png", 
+        "https://i.imgur.com/2LnK3RL.png",
+        "https://i.imgur.com/JLlpDa6.png",
+        "https://i.imgur.com/MO2ZfY9.png",
+        "https://i.imgur.com/GDeUThX.png",
+        "https://i.imgur.com/xnjtkzl.png", 
+        "https://i.imgur.com/JyBHQDr.png", 
+        "https://i.imgur.com/hObrat4.png"
+    ], // Images
+      [ 50, 
+        75, 
+        100,
+        125,
+        150,
+        175, 
+        200,
+        225,
+        250
+    ], // HP values
+      [ 250, 
+        225, 
+        200,
+        175,
+        150,
+        125,
+        100,
+        75,
+        50
+    ], // Attack damage values,
+    "Boss Tycoon", // Boss name
+    "https://i.imgur.com/VdMFlgf.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
@@ -20,6 +58,14 @@ const main = async () => {
     // Get the value of the NFT's URI.
     let returnedTokenUri = await gameContract.tokenURI(1);
     console.log("Token URI:", returnedTokenUri);
+
+
+
+    txn = await gameContract.attackBoss();
+await txn.wait();
+
+txn = await gameContract.attackBoss();
+await txn.wait();
   };
   
   const runMain = async () => {
